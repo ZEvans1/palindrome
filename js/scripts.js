c2c66286fdb3d9cc75a1879d75f95ccad364abba
Adam Calhoun and Zach Evans <zach@>
2017-12-05 15:21:06 -0800

    Add commented section about how to execute without regex.

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,25 @@ $(document).ready(function() {
 
     var userInput = $("#whatever").val().toLowerCase();
     var noPunc = userInput.replace(/[^a-z0-9]/gi, '');
+    //var alphanum = "abcdefghijklmnopqrstuvwxyz0987654321".split('')
     var letters = noPunc.split('');
 
     console.log(checkLetters(letters));
 
   });
 });
+//If we were to not use regex, we would use the following code:
+
+// var alphanum = "abcdefghijklmnopqrstuvwxyz0987654321".split('')
+// var array = []
+//
+// for (var i=0; i < userInput.length; i++) {
+// 	var char = userInput[i]
+//   if (alphanum.indexOf(char) !== -1){
+//   	array.push(char)
+//   }
+// }
+
 //BACKEND BELOW THIS LINE -------------------------
 function checkLetters(array){
   var letters = array
